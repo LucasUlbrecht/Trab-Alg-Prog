291b7696820de33190c6239db4f2bb6fbc5c4a63
Lucas Ulbrecht <98244995+LucasUlbrecht@users.noreply.github.com>
2022-10-18 16:25:43 -0400

    Atualização das funções

--- funcoes.cpp
+++ funcoes.cpp
@@ -1,13 +1,66 @@
 #include"funcoes.h"
 
-void trocaCursos(Curso* lcPesosInit, Curso* lcPesosEnd){
-    Curso aux;
-    aux = *lcPesosInit;
-    *lcPesosInit = *lcPesosEnd;
-    *lcPesosEnd = aux;
-    return;
+//Funções gerar lista
+
+void trocaCursos(Curso* lcPesosInit, Curso* lcPesosEnd);
+int organizaCurso(Curso* lcPesos, int init, int end);
+void quickSortCurso(Curso* lcPesos, int init, int end);
+void gerarListaCursos(FILE** cursosPesos, FILE** cursosVagas, Curso* lcPesos);
+void gerarListaAluno();
+int lerArquivos(FILE* acertos, FILE* cursosPesos, FILE* cursosVagas, FILE* dados, Curso* lcCurso){
+    acertos = fopen("acertos.txt", "r");
+    if(acertos==NULL){
+        return 1;
+    }
+    cursosPesos = fopen("cursos_e_pesos.txt", "r");
+        if(cursosPesos==NULL){
+            fclose(acertos);
+            return 1;
+    }
+    cursosVagas = fopen("cursos_e_vagas.txt", "r");
+        if(cursosVagas==NULL){
+            fclose(acertos);
+            fclose(cursosPesos);
+            return 1;
+    }
+    dados = fopen("dados.txt", "r");
+        if(dados==NULL){
+            fclose(acertos);
+            fclose(cursosPesos);
+            fclose(cursosVagas);
+            return 1;
+    }
+    gerarListaCursos(&cursosPesos, &cursosVagas, lcCurso);
+    //gerarListaAluno();
+    return 0;
 }
 
+
+void gerarListaCursos(FILE** cursosPesos, FILE** cursosVagas, Curso* lcPesos){ // lcPesos = lista curso pesos
+    FILE* aux;
+    int n;
+    aux = *cursosPesos;
+    fscanf(aux, "%d", n);
+    lcPesos=(Curso*) calloc(n, sizeof(Curso));
+    for(int cont = 0; feof(aux) != NULL && cont<n ; cont++){
+        fscanf(aux, "%d %[^-] %s %d %d %d %d %d", &lcPesos[cont].codigo, &lcPesos[cont].nome, &lcPesos[cont].tipoCurso, &lcPesos[cont].pRED, &lcPesos[cont].pMAT, &lcPesos[cont].pLIN, &lcPesos[cont].pHUM, &lcPesos[cont].pNAT);
+    }
+    aux = *cursosVagas;
+    quickSortCurso(lcPesos, 0, n-1);
+    //for(int cont = 0; feof(aux) != NULL && cont<n ; cont++){
+        
+    //}
+}
+
+
+void quickSortCurso(Curso* lcPesos, int init, int end){
+    int n;
+    if (init<end){
+        n=organizaCurso(lcPesos, 0, end);
+        quickSortCurso(lcPesos, 0, n);
+        quickSortCurso(lcPesos, n+1, end);
+    }
+}
 int organizaCurso(Curso* lcPesos, int init, int end){
     int aux, i, j; 
     i= init-1;
@@ -33,78 +86,17 @@ int organizaCurso(Curso* lcPesos, int init, int end){
     }
 }
 
-void quickSortCurso(Curso* lcPesos, int init, int end){
-    int n;
-    if (init<end){
-        n=organizaCurso(lcPesos, 0, end);
-        quickSortCurso(lcPesos, 0, n);
-        quickSortCurso(lcPesos, n+1, end);
-    }
-}
-
-void gerarListaCursos(FILE** cursosPesos, FILE** cursosVagas, Curso* lcPesos){ // lcPesos = lista curso pesos
-    FILE* aux;
-    int n;
-    aux = *cursosPesos;
-    fscanf(aux, "%d", n);
-    lcPesos=(Curso*) calloc(n, sizeof(Curso));
-    for(int cont = 0; feof(aux) != NULL && cont<n ; cont++){
-        fscanf(aux, "%d", lcPesos[cont].codigo);
-        fscanf(aux, "%[^-]", lcPesos[cont].nome);
-        fscanf(aux, "%s", lcPesos[cont].tipoCurso);
-        fscanf(aux, "%d", lcPesos[cont].pRED);
-        fscanf(aux, "%d", lcPesos[cont].pMAT);
-        fscanf(aux, "%d", lcPesos[cont].pLIN);
-        fscanf(aux, "%d", lcPesos[cont].pHUM);
-        fscanf(aux, "%d", lcPesos[cont].pNAT);
-    }
-    aux = *cursosVagas;
-    quickSortCurso(lcPesos, 0, n-1);
-    //for(int cont = 0; feof(aux) != NULL && cont<n ; cont++){
-        
-    //}
-}
-
-void gerarListaAluno();
-
-int lerArquivos(FILE* acertos, FILE* cursosPesos, FILE* cursosVagas, FILE* dados, Curso* lcCurso){
-    acertos = fopen("acertos.txt", "r");
-    if(acertos==NULL){
-        return 1;
-    }
-    cursosPesos = fopen("cursos_e_pesos.txt", "r");
-        if(cursosPesos==NULL){
-            fclose(acertos);
-            return 1;
-    }
-    cursosVagas = fopen("cursos_e_vagas.txt", "r");
-        if(cursosVagas==NULL){
-            fclose(acertos);
-            fclose(cursosPesos);
-            return 1;
-    }
-    dados = fopen("dados.txt", "r");
-        if(dados==NULL){
-            fclose(acertos);
-            fclose(cursosPesos);
-            fclose(cursosVagas);
-            return 1;
-    }
-    gerarListaCursos(&cursosPesos, &cursosVagas, lcCurso);
-    gerarListaAluno();
-    return 0;
-}
 
 
 
+//Funções encerrar lista
 
-
-
-
-void encerrarPrograma(FILE** acertos, FILE** cursosPesos, FILE** cursosVagas, FILE** dados){
+void encerrarListaCursos(Curso* lcCurso);
+void encerrarPrograma(FILE** acertos, FILE** cursosPesos, FILE** cursosVagas, FILE** dados, Curso* lcCurso){
     fclose(*acertos);
     fclose(*cursosPesos);
     fclose(*cursosVagas);
     fclose(*dados);
+    //encerrarListaCursos(lcCurso);
     return;
 }
--- funcoes.h
+++ funcoes.h
@@ -39,9 +39,20 @@ struct Aluno{
 };
 
 
+// Funções ler arquivos
 int lerArquivos(FILE* acertos, FILE* cursosPesos, FILE* cursosVagas, FILE* dados, struct Curso* lcCurso);
+void gerarListaCursos(FILE** cursosPesos, FILE** cursosVagas, Curso* lcPesos);
+void quickSortCurso(Curso* lcPesos, int init, int end);
+int organizaCurso(Curso* lcPesos, int init, int end);
+void trocaCursos(Curso* lcPesosInit, Curso* lcPesosEnd);
+
+
+
 void retornarArquivos();
 int pesquisarPessoa();
 void listarReprovados();
 void listarAprovados();
-void encerrarPrograma(FILE** acertos, FILE** cursosPesos, FILE** cursosVagas, FILE** dados);
+
+//Funções encerrar programa
+void encerrarPrograma(FILE** acertos, FILE** cursosPesos, FILE** cursosVagas, FILE** dados, Curso* lcCurso);
+void encerrarListaCursos(Curso* lcCurso);
